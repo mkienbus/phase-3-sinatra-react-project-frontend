9bafab52d766ed7c120420a59a1d63714979b56c
Michael Kienbsuch <mkienbus@gmail.com>
2021-12-03 18:36:20 -0700

    setting up CountryForm functionality

--- src/CountryForm.js
+++ src/CountryForm.js
@@ -1,10 +1,17 @@
-
+import React, { useState } from "react"
 
 function CountryForm(){
+    const [countryName, setCountryName] = useState("")
+    const [continentId, setContinentId] = useState(0)
 
     return (
         <div>
-            <h3>Country Form</h3>
+            <h3>Add a new country to your list</h3>
+                <form>
+                    <label>Country Name:</label>
+                    <input type = "text" name = "countryName" value = {countryName} onChange = {e => setCountryName(e.target.value)}/>
+                    <button type = "submit">Add Country</button>
+                </form>
         </div>
     )
 
