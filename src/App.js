a18aa4c7f1ca5ad8df71ed60ee718cb4c72e52cd
Michael Kienbsuch <mkienbus@gmail.com>
2021-12-05 15:00:32 -0700

    added a table component, stylized with css, rendered in App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Countries from "./Countries"
 import CountryForm from "./CountryForm"
+import ContinentIdTable from "./ContinentIdTable";
 
 function App() {
 
@@ -25,6 +26,7 @@ useEffect(() => {
       <h1>Welcome to Geotrack, the app which helps you keep track of the countries you've visited!</h1>
       <Countries countries = {countries}/>
       <CountryForm />
+      <ContinentIdTable />
     </div>
   );
 }
---
+++ src/ContinentIdTable.js
@@ -0,0 +1,43 @@
+
+import React from "react"
+
+function ContinentIdTable(){
+    return(
+        <table id = "table">
+        <tr>
+          <th>Continent</th>
+          <th>Continent ID</th>
+        </tr>
+        <tr>
+          <td>North America</td>
+          <td>1</td>
+        </tr>
+        <tr>
+          <td>South America</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>Africa</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>Europe</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <td>Asia</td>
+          <td>5</td>
+        </tr>
+        <tr>
+          <td>Oceania</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>Antarctica</td>
+          <td>7</td> 
+        </tr>
+      </table>
+    )
+}
+
+export default ContinentIdTable;
