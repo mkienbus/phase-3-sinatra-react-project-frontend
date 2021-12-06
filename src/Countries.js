004726c1b7fabdd5d866bca6bd1eda9359be526a
Michael Kienbsuch <mkienbus@gmail.com>
2021-12-05 18:24:48 -0700

    error fix, added unique id's to cards for react

--- src/Countries.js
+++ src/Countries.js
@@ -4,7 +4,7 @@ import Country from "./Country"
 // container component to render Country
 function Countries ({countries}){
     console.log(countries)
-    const allCountries = countries.map((c) => <Country country = {c}/>)
+    const allCountries = countries.map((c) => <Country country = {c} key = {c.id}/>)
     return(
         <div>
             <h3>Countries:</h3>
